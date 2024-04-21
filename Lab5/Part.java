d7bbfe184716f3560b54688753604581f756a707
Ivan2599 <evrjmet@yandex.ru>
2024-04-21 16:27:12 +0300

    lab5 complete

---
+++ Lab5/Part.java
@@ -0,0 +1,61 @@
+public class Part {
+    // 1
+    public static String findLongestWord(String text) {
+        String[] words = text.split("\\s+");
+        String longestWord = "";
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
+
+    // 2
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    // 3
+    public static String censorWord(String text) {
+        return text.toLowerCase().replaceAll("бяка", "[вырезано цензурой]");
+    }
+
+    // 4
+    public static int countOccurrences(String text, String substring) {
+        int count = 0;
+        int index = 0;
+        while ((index = text.indexOf(substring, index)) != -1) {
+            count++;
+            index += substring.length();
+        }
+        return count;
+    }
+
+    // 5
+    public static String invertWords(String inputString) {
+        String[] words = inputString.split("\\s+");
+        String inverted = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            inverted += reverseWord(words[i]) + " ";
+        }
+        return inverted.trim();
+    }
+
+    private static String reverseWord(String word) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+        return reversed;
+    }
+}
